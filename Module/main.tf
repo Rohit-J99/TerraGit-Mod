a8f8e5b384187a52e24c9ead983cdd61ff49cd43
Rohit_J <87205506+Rohit-J99@users.noreply.github.com>
2023-03-22 22:26:56 +0530

    Update main.tf

--- Module/main.tf
+++ Module/main.tf
@@ -12,6 +12,10 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = "849d2a88-1ba7-4417-9f64-86f2dd020dde"
+  tenant_id       = "58f65d12-7e18-4e74-a18b-5ffc7b030878"
+  client_id       = "06e718c6-daea-46ca-a946-c70a7885d7a1"
+  client_secret   = "MNy8Q~T533W~Ymu4mgq5Q~tjoAX7QVqmgDhjXbul"
 }
 
 
